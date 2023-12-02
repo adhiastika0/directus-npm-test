720110f90beb2247f464074b97fd1ca5a9296709
Justicly <adhiastika19@gmail.com>
2023-12-03 02:16:27 +0800

    change index,js and vercel.json config

--- api/index.js
+++ api/index.js
@@ -1,3 +1,12 @@
-import app from '../src/app';
+const { execSync } = require('child_process');
 
-export default app;
+module.exports = async (req, res) => {
+    try {
+        // Jalankan perintah directus start
+        execSync('npx directus start', { stdio: 'inherit' });
+        res.status(200).send('Directus started successfully.');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error starting Directus.');
+    }
+};
